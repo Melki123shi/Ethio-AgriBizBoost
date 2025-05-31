6281e739cb0d212fbd8f6f3f01de4ac096444a00
sosinaesayas <sosinaesayas@gmail.com>
2025-05-31 10:50:43 +0300

    saved loan advice and forcasting to database

--- backend/forcasting/routes.py
+++ backend/forcasting/routes.py
@@ -1,14 +1,23 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from forcasting.model import PredictionInput
 from forcasting.services import make_predictions
+from forcasting.database import save_prediction_result
+from auth.dependencies import get_current_active_user
 
 router = APIRouter()
 
 @router.post("/predict", response_model=dict)
-async def predict(data: PredictionInput):
+async def predict(
+    data: PredictionInput,
+    current_user: dict = Depends(get_current_active_user)
+):
     try:
-        serilized_data = data.model_dump()
-        result = make_predictions(serilized_data)
+        serialized_data = data.model_dump()
+        result = make_predictions(serialized_data)
+        
+        user_id = current_user["_id"]
+        save_prediction_result(user_id, serialized_data, result)
+
         return {"success": True, "data": result}
     except Exception as e:
         return {"success": False, "error": str(e)}
